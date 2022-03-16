e6b4a223082619f856f1cedfef4ff057668f3c69
a-jwc <chauajw@gmail.com>
2022-03-15 23:48:06 -0700

    add get and update avatar image endpoints

---
+++ prisma/migrations/20220315194251_add_likes_and_avatar/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Sound" ADD COLUMN     "likes" INTEGER DEFAULT 1;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "avatar" TEXT;
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,23 @@
-import { Controller, Get, UseGuards, Request, Res, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Request,
+  Res,
+  Req,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+  HttpStatus,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { UsersService } from './users.service';
+import { v4 as uuidv4 } from 'uuid';
+import { Response } from 'express';
+import { statSync } from 'fs';
+import { join } from 'path';
 
 @Controller('api/v1/user')
 export class UserController {
@@ -16,8 +33,39 @@ export class UserController {
   }
 
   @UseGuards(JwtAuthGuard)
-  @Get('isLoggedIn')
+  @Get('is-logged-in')
   isLoggedIn(@Req() req, @Res() res) {
     return res.send(true);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('update-avatar')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './public/images',
+        filename: (_req, file, cb) => {
+          const ext = file.mimetype.split('/')[1];
+          cb(null, `${uuidv4()}-${Date.now()}.${ext}`);
+        },
+      }),
+    }),
+  )
+  async updateAvatar(@Req() req, @UploadedFile() file: Express.Multer.File) {
+    const avatar = await this.userService.updateUser({
+      where: { id: req.user.id },
+      data: { avatar: file.filename },
+    });
+    return avatar;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('get-avatar')
+  async getAvatar(@Req() req, @Res() res: Response) {
+    const { avatar } = await this.userService.user({
+      id: req.user.id,
+    });
+    if (avatar === null) return res.sendStatus(HttpStatus.NOT_FOUND);
+    return res.sendFile(avatar, { root: 'public/images' });
   }
 }
