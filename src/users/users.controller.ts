6583a93bfb4c6eef32a2b7af695ae28da246f1ed
a-jwc <chauajw@gmail.com>
2022-03-18 18:57:01 -0700

    add update-user info endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,8 @@ import {
   UploadedFile,
   UseInterceptors,
   HttpStatus,
+  Body,
+  Put,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
@@ -16,10 +18,15 @@ import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { UsersService } from './users.service';
 import { v4 as uuidv4 } from 'uuid';
 import { Response } from 'express';
+import { Prisma } from '@prisma/client';
+import { SoundsService } from 'src/sounds/sounds.service';
 
 @Controller('api/v1/user')
 export class UserController {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly soundsService: SoundsService,
+  ) {}
 
   @UseGuards(JwtAuthGuard)
   @Get('profile')
@@ -33,7 +40,6 @@ export class UserController {
   @UseGuards(JwtAuthGuard)
   @Get('likes')
   async getLikes(@Req() req) {
-    const userId: number = req.user.id;
     const result = await this.userService.getUserLikesSoundObject({
       id: req.user.id,
     });
@@ -75,5 +81,28 @@ export class UserController {
     });
     if (avatar === null) return res.sendStatus(HttpStatus.NOT_FOUND);
     return res.sendFile(avatar, { root: 'public/images' });
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Put('update-user')
+  async updateUserInfo(
+    @Req() req,
+    @Res() res: Response,
+    @Body() data: Prisma.UserUpdateInput,
+  ) {
+    await this.userService.updateUser({
+      where: { id: req.user.id },
+      data,
+    });
+    if (data.username) {
+      const updateUserSounds: Prisma.SoundUpdateManyWithoutAuthorInput = {
+        updateMany: { where: {}, data: { authorName: data.username } },
+      };
+      await this.userService.updateUser({
+        where: { id: req.user.id },
+        data: { sounds: updateUserSounds },
+      });
+    }
+    return res.sendStatus(HttpStatus.OK);
   }
 }
